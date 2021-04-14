99837360b1bcd57b4d477bd4e7fc73e823885832
scal9000 <scalettamarco@yahoo.it>
2021-04-14 14:20:13 +0200

    Provable Contract Old re-assignment

--- BankAccount/Test_Problem/Account.java
+++ BankAccount/Test_Problem/Account.java
@@ -1,10 +1,17 @@
 
 interface AccountI{
 
-//@accessible \inv: \dl_account_fields;
+	//@accessible \inv: \dl_account_fields;
 
 	//@assignable \strictly_nothing;
 	//@accessible \dl_account_fields;
+	//@ensures \result == getBalance();
 	int /*@ pure @*/ getBalance();
+
+
+	//@assignable \dl_account_fields;
+	//@accessible \dl_account_fields;
+	//@ensures this.getBalance() == newBalance;
+	void setBalance(int newBalance);
 	
 }
--- BankAccount/Test_Problem/Application.java
+++ BankAccount/Test_Problem/Application.java
@@ -2,49 +2,26 @@
 
 	//@invariant \invariant_for(account);
 	AccountI account;
-  private int oldGetBalance;
 
-  //@requires \disjoint(this.oldGetBalance, \dl_account_fields);
-	//@ensures \old(account.getBalance()) == account.getBalance();
-	void unprovable_regular_old() {
-    /*@ ae_constraint \disjoint(\dl_frame, this.account) 
-        && \disjoint(\dl_frame, this) 
-        && \disjoint(\dl_frame, this.oldGetBalance)
-        && \disjoint(\dl_frame, \dl_account_fields) ;
-    @*/
-    { ; }
-		/*@
-      @ assignable \dl_frame;
-      @ exceptional_behavior requires false;
-      @ break_behavior requires false;
-      @ continue_behavior requires false;
-      @ return_behavior requires false;
-      @ normal_behavior ensures \invariant_for(this);
-      @*/
-    \abstract_statement Original;
+    //@requires \disjoint(this.account, \dl_account_fields);
+	//@ensures \old(account.getBalance()) > account.getBalance();
+	void provable_complex_old() {
+        /*@ ae_constraint \disjoint(\dl_frame, this.account) 
+            && \disjoint(\dl_frame, \dl_account_fields) ;
+        @*/
+        { ; }
+            /*@
+        @ assignable \dl_frame;
+        @ exceptional_behavior requires false;
+        @ break_behavior requires false;
+        @ continue_behavior requires false;
+        @ return_behavior requires false;
+        @ normal_behavior ensures \invariant_for(this);
+        @*/
+        \abstract_statement Original;
+        account.setBalance(account.getBalance() - 1);
 	}
 
-
-  //@requires \disjoint(this.oldGetBalance, \dl_account_fields);
-	//@ensures oldGetBalance == account.getBalance();
-	void unprovable_workaround() {
-    oldGetBalance = account.getBalance(); 
-    /*@ ae_constraint \disjoint(\dl_frame, this.account) 
-        && \disjoint(\dl_frame, this) 
-        && \disjoint(\dl_frame, this.oldGetBalance)
-        && \disjoint(\dl_frame, \dl_account_fields) ;
-    @*/
-    { ; }
-		/*@
-      @ assignable \dl_frame;
-      @ exceptional_behavior requires false;
-      @ break_behavior requires false;
-      @ continue_behavior requires false;
-      @ return_behavior requires false;
-      @ normal_behavior ensures \invariant_for(this);
-      @*/
-    \abstract_statement Original;
-	}
 
 }
 
