69242a0b031c498d5120047a5f4b56bd36ffc4a4
scal9000 <scalettamarco@yahoo.it>
2021-05-10 12:04:18 +0200

    Add SPL to be model

---
+++ MinePumpPL/features/highWaterSensor/MinePumpSystem/MinePump.java
@@ -0,0 +1,19 @@
+package MinePumpSystem;
+
+import MinePumpSystem.Environment;
+
+public class MinePump {
+
+	public void processEnvironment() {
+		if (!pumpRunning && isHighWaterLevel()) {
+			activatePump();
+			original();
+		} else {
+			original();
+		}
+	}
+	
+	boolean isHighWaterLevel() {
+		return !env.isHighWaterSensorDry();
+	}
+}
